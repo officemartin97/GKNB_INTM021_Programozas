8d7dcaa7dfd444415a46cee506326107a9c244e0
officemartin97 <officemartin97@gmail.com>
2022-12-20 15:42:43 +0100

    stuck on maxNumberOfEachChar;

--- solutions/gknb_intm021-2019_20_1-01-HFM-1220/main.cpp
+++ solutions/gknb_intm021-2019_20_1-01-HFM-1220/main.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    int a;
+    std::vector<std::vector<char>> t;
+    srand((unsigned int)time(NULL));
+
+    // Initializing the <vector> table of characters:
+    for(int i=0; i < 2; i++)
+    {
+        std::vector<char> row;
+        for(int j = 0; j < 3; j++)
+        {
+            int randomCharCounter[3] = {3,3,3};
+            char randomChar = 'a' + rand()%3;
+
+            
+
+            row.push_back(randomChar);
+        }
+        t.push_back(row);
+    }
+
+    // Print out the <vector> table of characters:
+    for(const auto &row : t)
+    {
+        for(const auto &c : row)
+        {
+            std::cout << c <<' ';
+        }
+        std::cout << std::endl;
+    }
+
+
+
+    /*int a;
     int b;
-    std::cout<< "Sor: ";
+    std::cout << "Memoriajatek"<< std::endl << "____________" << std::endl;
+    std::cout << "Sor: ";
     std::cin >> a;
-    std::cout<< "Oszlop: ";
-    std::cin >> b;
+    std::cout << "Oszlop: ";
+    std::cin >> b;*/
 }
